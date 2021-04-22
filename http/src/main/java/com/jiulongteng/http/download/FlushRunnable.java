5986b6733bf4f7d42d45718bcdf8d30aa4861141
jiulongteng <jiulongteng@myweimai.com>
2021-04-22 19:37:10 +0800

    优化同步磁盘I机制

--- http/src/main/java/com/jiulongteng/http/download/FlushRunnable.java
+++ http/src/main/java/com/jiulongteng/http/download/FlushRunnable.java
@@ -1,14 +1,17 @@
 package com.jiulongteng.http.download;
 
+import com.jiulongteng.http.download.db.DownloadCache;
+
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
-import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.concurrent.locks.LockSupport;
 
@@ -22,16 +25,15 @@ public class FlushRunnable implements Runnable {
 
     private DownloadTask task;
     ConcurrentHashMap<Integer, AbstractDownloadRunnable> downloadRunnableMap = new ConcurrentHashMap<>();
-    LinkedBlockingQueue<Runnable> blockingQueue = new LinkedBlockingQueue<>();
-    int finishSize = 0;
     AtomicReference<Thread> parkThreadRef = new AtomicReference(null);
 
     private volatile Future syncFuture;
 
-    AtomicBoolean isDone = new AtomicBoolean(false);
+    AtomicLong allNoSyncLength = new AtomicLong(0);
 
+    HashSet<Integer> finishedIndex = new HashSet<>();
     Runnable finishRunnable;
-
+    long memoryBufferSize = 8096;
     public FlushRunnable(DownloadTask task, Runnable finishRunnable) {
         this.task = task;
         this.finishRunnable = finishRunnable;
@@ -39,19 +41,19 @@ public class FlushRunnable implements Runnable {
 
     @Override
     public void run() {
-        flushAll();
         parkThreadRef.set(Thread.currentThread());
+        flushAll();
         while (true) {
-            parkThread();
+            parkThread(300);
             flushAll();
             if (task.isStoped()) {
-                Util.i(TAG, "   loop " + finishSize);
+                Util.i(TAG,  downloadRunnableMap.size() + " all size " + task.getRunnableSize()+ "   loop " + finishedIndex.size());
             }
-            if (task.getRunnableSize() == finishSize) {
+            if (task.getRunnableSize() == finishedIndex.size()) {
                 if (finishRunnable != null) {
                     FILE_IO_EXECUTOR.submit(finishRunnable);
                 }
-                Util.i(TAG, " finishSize ");
+                Util.i(TAG, " finishSize " + finishedIndex.size());
                 break;
             }
         }
@@ -78,63 +80,52 @@ public class FlushRunnable implements Runnable {
     }
 
     private void flushAll() {
-
-        if (isDone.get()) {
-            while (true) {
-                Runnable runnable = blockingQueue.poll();
-                if (runnable != null) {
-                    runnable.run();
-                }
-
-                if (task.getRunnableSize() == finishSize) {
-                    break;
-                } else {
-                }
-            }
-        } else {
-            for (int i = 0; i < task.getRunnableSize(); i++) {
-                AbstractDownloadRunnable downloadRunnable = downloadRunnableMap.remove(i);
-                if (downloadRunnable != null) {
-                    try {
+        HashMap<Integer,AbstractDownloadRunnable> errorMap =new HashMap<>();
+        boolean needSyncBuffer = allNoSyncLength.get() >= DownloadCache.getInstance().getSyncBufferSize();
+        boolean synced = false;
+        for (int i = 0; i < task.getRunnableSize(); i++) {
+            AbstractDownloadRunnable downloadRunnable = downloadRunnableMap.remove(i);
+            if (downloadRunnable != null) {
+                boolean isReadByteFinished = downloadRunnable.getIsReadByteFinished();
+                try {
+                    if(isReadByteFinished || needSyncBuffer){
+                        synced = true;
                         downloadRunnable.flush();
-                    } catch (IOException e) {
-                        e.printStackTrace();
                     }
-
+                } catch (IOException e) {
+                    Util.e(TAG, "buffered "+downloadRunnable.getBufferedLength() +" AbstractDownloadRunnable flush error index = " + downloadRunnable.getIndex(), e);
+                    e.printStackTrace();
+                    errorMap.put(i,downloadRunnable);
+                }finally {
+                    if(isReadByteFinished){
+                        finishedIndex.add(i);
+                        downloadRunnable.unPark();
+                        Util.i(TAG, "  finishSize = " + finishedIndex.size() + "  index = " + downloadRunnable.getIndex());
+                    }
                 }
-
             }
         }
+        if(synced){
+            task.getCallbackDispatcher().fetchProgress(task);
+        }
+//        if(!errorMap.isEmpty()){
+//            downloadRunnableMap.putAll(errorMap);
+//        }else {
+//        }
+
 
 
     }
 
-    public void flush(AbstractDownloadRunnable downloadRunnable) {
+    public void flush(AbstractDownloadRunnable downloadRunnable,long byteRead) {
         downloadRunnableMap.put(downloadRunnable.getIndex(), downloadRunnable);
+        allNoSyncLength.addAndGet(byteRead);
         wakeup();
     }
 
     public void done(AbstractDownloadRunnable downloadRunnable) {
-        isDone.set(true);
-        blockingQueue.offer(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    downloadRunnable.flush();
-
-                } catch (IOException e) {
-
-                } finally {
-                    finishSize++;
-                    downloadRunnable.unPark();
-                    Util.i(TAG, "  finishSize = " + finishSize + "  done " + downloadRunnable.getIndex());
-                }
-
-            }
-        });
+        downloadRunnableMap.put(downloadRunnable.getIndex(),downloadRunnable);
         wakeup();
-
-
     }
 
     // convenient for test
